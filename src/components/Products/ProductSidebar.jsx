dcf8f0cde1e06d07170b5ed08b223216a2d8e183
oaa-dev <oaadevelopment.acc@gmail.com>
2025-01-18 00:17:00 +0800

    set navigation category

--- src/components/Products/ProductSidebar.jsx
+++ src/components/Products/ProductSidebar.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductSidebar.css";
 
 import rocket from "../../assets/img/rocket.png";
 
 import LinkWithIcon from "../Navbar/LinkWithIcon";
+import apiClient from "../../utils/api-client";
 
 const ProductSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState([]);
+
+  useEffect(() => {
+    apiClient
+      .get("/category")
+      .then((res) => setCategories(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
   return (
     <aside className="product_sidebar">
       <h2>Category</h2>
       <div className="category_links">
-        <LinkWithIcon
-          title="Electronics"
-          link="products?category=electronics"
-          emoji={rocket}
-          is_sidebar={true}
-        />
+        {error && <em className="form_error">{error}</em>}
+        {categories.map((category) => (
+          <LinkWithIcon
+            id={category.id}
+            title={category.name}
+            link={`products?category=${category.name}`}
+            emoji={`http://localhost:5000/category/${category.image}`}
+            is_sidebar={true}
+          />
+        ))}
       </div>
     </aside>
   );
