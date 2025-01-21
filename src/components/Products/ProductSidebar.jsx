1a3566b4f2f42075c1a794d495639f7aaa1a06ab
oaa-dev <oaadevelopment.acc@gmail.com>
2025-01-21 21:24:13 +0800

    hooks

--- src/components/Products/ProductSidebar.jsx
+++ src/components/Products/ProductSidebar.jsx
@@ -1,36 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./ProductSidebar.css";
-
-import rocket from "../../assets/img/rocket.png";
 
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 import apiClient from "../../utils/api-client";
+import useData from "../../hooks/useData";
 
 const ProductSidebar = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState([]);
-
-  useEffect(() => {
-    apiClient
-      .get("/category")
-      .then((res) => setCategories(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { data: categories, error } = useData("/category");
 
   return (
     <aside className="product_sidebar">
       <h2>Category</h2>
       <div className="category_links">
         {error && <em className="form_error">{error}</em>}
-        {categories.map((category) => (
-          <LinkWithIcon
-            id={category.id}
-            title={category.name}
-            link={`products?category=${category.name}`}
-            emoji={`http://localhost:5000/category/${category.image}`}
-            is_sidebar={true}
-          />
-        ))}
+        {categories &&
+          categories.map((category) => (
+            <LinkWithIcon
+              id={category.id}
+              title={category.name}
+              link={`products?category=${category.name}`}
+              emoji={`http://localhost:5000/category/${category.image}`}
+              is_sidebar={true}
+            />
+          ))}
       </div>
     </aside>
   );
---
+++ src/hooks/useData.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../utils/api-client.js";
+
+const useData = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get(url)
+      .then((res) => setData(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return { data, error };
+};
+
+export default useData;
